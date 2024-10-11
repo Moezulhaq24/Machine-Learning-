402b0fb671389370111c53849e8ade0f7dc8bde5
Moezulhaq24 <moizulhaq331@gmail.com>
2024-10-11 17:44:05 +0500

    Printing only the id's and email of the persons from the data

--- Reading From API.py
+++ Reading From API.py
@@ -10,5 +10,21 @@ response = requests.get(url+"/api/users?page=2").json()
 
 data = response["data"]
 
+# Printing the specific data from website using API
+# for datum in data:
+#     print(datum)
+
+ids = []
+emails = []
+
+# Printing only the id's and email of the persons from the data
 for datum in data:
-    print(datum)
+    ids.append(datum['id'])
+    emails.append(datum['email'])
+
+for id in ids:
+    print(id)
+
+for email in emails:
+    print(email)
+
