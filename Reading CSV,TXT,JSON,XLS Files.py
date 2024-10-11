398184be1dd6cea4187143ec2c4ec5b3f4d7f761
Moezulhaq24 <moizulhaq331@gmail.com>
2024-10-11 15:12:02 +0500

    loadFiles function

--- Reading CSV,TXT,JSON,XLS Files.py
+++ Reading CSV,TXT,JSON,XLS Files.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 # Make a menu function for the CSV,JSON,TXT and XLS files
 
@@ -9,5 +10,29 @@ def menu():
     print("Press 4 for XLS File")
 
     return int(input("Enter the number according to the file you want to upload : "))
+
+def loadFiles():
+    file = menu()
+    path = input(f"Enter the path of the file you want to upload : ")
+
+    if file == 1:
+        df = pd.read_csv(path)
+    
+    elif file == 2:
+        with open(path,'r') as jsonFile:
+            df = json.load(jsonFile)
+    
+    elif file == 3:
+        with open(path,'r') as txtFile:
+            df = file.open()
+
+    elif file == 4:
+        df = pd.read_excel(path)
+
+    else:
+        return "The file type is not supported."
+    
+    return df
+
 
 
